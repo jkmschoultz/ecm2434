fcb8f0b8016e615c58db9275f575d718f2c35b6a
Johan Schoultz <jkmschoultz@gmail.com>
2023-02-17 12:57:38 +0000

    Created database tables

--- API/database/models.py
+++ API/database/models.py
@@ -18,3 +18,33 @@ class Building(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     radius = models.FloatField()
+
+class Question(models.Model):
+    question = models.CharField(max_length=255)
+
+class Answer(models.Model):
+    text = models.CharField(max_length=255)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    is_correct = models.BooleanField()
+
+class HasAnswered(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+
+class Leaderboard(models.Model):
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_points_in_building = models.PositiveIntegerField()
+
+class Achievement(models.Model):
+    challenge = models.CharField(max_length=255)
+    xp_reward = models.PositiveIntegerField()
+    points_reward = models.PositiveIntegerField()
+
+class UserAchievement(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+
+class Fountain(models.Model):
+    location = models.CharField(max_length=255)
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
