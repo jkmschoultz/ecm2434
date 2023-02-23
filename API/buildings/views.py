a7fef809f06e84df0d6f54119aedb70c80854271
Johan Schoultz <jkmschoultz@gmail.com>
2023-02-23 16:27:28 +0000

    Made user location data part of POST

--- API/buildings/views.py
+++ API/buildings/views.py
@@ -1,15 +1,25 @@
 import math
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404, HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from database.models import Building
 
 # Create building views here.
-def index(request, lat=50.73827, long=-3.53098):
-    # Default position to SWIOT if none given
+@csrf_exempt
+def index(request):
+    # Default position to Uni entrance if none given
+    lat=50.73505
+    long=-3.53207
+    if request.method == 'POST':
+        lat = float( request.POST['lat'] )
+        long = float( request.POST['long'] )
+        print(lat)
+        print(long)
+
+    # Calculate distance from each building
     buildings = Building.objects.values()
     for building in buildings:
-        # Calculate distance from each building
         distance = math.sqrt((building['latitude'] - lat)**2 + (building['longitude'] - long)**2)
         building['distance'] = distance
         building['is_accessible'] = distance <= building['radius']
@@ -22,7 +32,7 @@ def index(request, lat=50.73827, long=-3.53098):
         del building['longitude']
         del building['radius']
         del building['distance']
-    return HttpResponse(buildings[:6])
+    return JsonResponse({'data': buildings[:6]})
 
 def detail(request, building_id):
     building = get_object_or_404(Building, pk=building_id)
