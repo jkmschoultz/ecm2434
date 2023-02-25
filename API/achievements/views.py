bb8c9bd213a6596ad5821ae0bc73beb988a5cddd
TobySlump <toby.slump@gmail.com>
2023-02-25 19:55:08 +0000

    Bottles Achievements

--- API/achievements/views.py
+++ API/achievements/views.py
@@ -22,28 +22,97 @@ def totalFilledBottlesAchievementCheck(current_username):
             user = User.objects.get(username = current_username),
             achievement =Achievement.objects.get(challenge = "Fill up your first water bottle"))
         
-    
+        user = User.objects.get(username = current_username)
+        user.xp = user.xp + 25
+        user.points = user.points + 5
+        user.save()
+
     #Fill up 5 bottles
     if bottles >= 5:
 
         newUserAchievement = UserAchievement.objects.create(
             user = User.objects.get(username = current_username),
             achievement =Achievement.objects.get(challenge = "Fill up 5 bottles"))
+        
+        user = User.objects.get(username = current_username)
+        user.xp = user.xp + 20
+        user.points = user.points + 3
+        user.save()
 
     #Fill up 10 bottles
+    if bottles >= 10:
+
+        newUserAchievement = UserAchievement.objects.create(
+            user = User.objects.get(username = current_username),
+            achievement =Achievement.objects.get(challenge = "Fill up 10 bottles"))
+        
+        user = User.objects.get(username = current_username)
+        user.xp = user.xp + 30
+        user.points = user.points + 3
+        user.save()
 
 
     #Fill up 50 bottles
+    if bottles >= 50:
+
+        newUserAchievement = UserAchievement.objects.create(
+            user = User.objects.get(username = current_username),
+            achievement =Achievement.objects.get(challenge = "Fill up 50 bottles"))
+        
+        user = User.objects.get(username = current_username)
+        user.xp = user.xp + 50
+        user.points = user.points + 5
+        user.save()
 
     
     #Fill up 100 bottles
+    if bottles >= 100:
+
+        newUserAchievement = UserAchievement.objects.create(
+            user = User.objects.get(username = current_username),
+            achievement =Achievement.objects.get(challenge = "Fill up 100 bottles"))
+        
+        user = User.objects.get(username = current_username)
+        user.xp = user.xp + 75
+        user.points = user.points + 5
+        user.save()
 
 
     #Fill up 250 bottles
+    if bottles >= 100:
+
+        newUserAchievement = UserAchievement.objects.create(
+            user = User.objects.get(username = current_username),
+            achievement =Achievement.objects.get(challenge = "Fill up 250 bottles"))
+        
+        user = User.objects.get(username = current_username)
+        user.xp = user.xp + 100
+        user.points = user.points + 10
+        user.save()
 
 
     #Fill up 500 bottles
+    if bottles >= 500:
+
+        newUserAchievement = UserAchievement.objects.create(
+            user = User.objects.get(username = current_username),
+            achievement =Achievement.objects.get(challenge = "Fill up 500 bottles"))
+        
+        user = User.objects.get(username = current_username)
+        user.xp = user.xp + 200
+        user.points = user.points + 15
+        user.save()
 
 
     #Fill up 1000 bottles
+    if bottles >= 1000:
+
+        newUserAchievement = UserAchievement.objects.create(
+            user = User.objects.get(username = current_username),
+            achievement =Achievement.objects.get(challenge = "Fill up 1000 bottles"))
+        
+        user = User.objects.get(username = current_username)
+        user.xp = user.xp + 300
+        user.points = user.points + 25
+        user.save()
 
