8d090f4c85713e21fabf4f4cc1d0d1a325ba7f32
TobySlump <toby.slump@gmail.com>
2023-02-26 15:48:39 +0000

    condensed function with validation

--- API/achievements/views.py
+++ API/achievements/views.py
@@ -8,8 +8,34 @@ from database.models import UserAchievement,User,Achievement
 
 
 def detail(request, current_username):
-    totalFilledBottlesAchievementCheck(current_username)
+    totalFilledBottlesAchievementCheck2(current_username)
     return JsonResponse({})
+
+def totalFilledBottlesAchievementCheck2(current_username):
+    user = User.objects.get(username = current_username)
+    bottles = user.bottles
+    listOfAchievements = UserAchievement.objects.all()
+    listOfBottleCheckpoints = [1,5,10,50,100,250,500,1000]
+
+    for checkpoint in listOfBottleCheckpoints:
+        if bottles >= checkpoint:
+            if checkpoint == 1:
+                challenge = "Fill up your first water bottle"
+            else:
+                challenge = "Fill up " + str(checkpoint) + " bottles"
+
+            achievement = Achievement.objects.get(challenge = challenge)
+            try:
+                UserAchievement.objects.get(user = user, achievement = achievement)
+            except:
+                newUserAchievement = UserAchievement.objects.create(
+                    user = user,
+                    achievement = achievement)
+            
+                user.xp = user.xp + achievement.xp_reward
+                user.points = user.points + achievement.points_reward
+                user.save()
+
 
 def totalFilledBottlesAchievementCheck(current_username):
     bottles = User.objects.get(username = current_username).bottles
