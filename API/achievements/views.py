abe19c3d5ecdb3ecf176d57171d8d6d4189301c8
TobySlump <toby.slump@gmail.com>
2023-02-28 15:41:22 +0000

    proper Json responses

--- API/achievements/views.py
+++ API/achievements/views.py
@@ -8,13 +8,32 @@ from database.models import UserAchievement,User,Achievement
 
 
 def detail(request, current_username):
-    totalFilledBottlesAchievementCheck(current_username)
-    return JsonResponse({})
+    dictOfNewAchievements = totalFilledBottlesAchievementCheck(current_username)
+    return JsonResponse(dictOfNewAchievements)
+
+def all(request, current_username):
+    dictOfAchievements = getAllUserAchievements(current_username)
+    return JsonResponse({"data" : dictOfAchievements})
+
+def getAllUserAchievements(current_username):
+    dictOfAchievements = []
+    user = User.objects.get(username = current_username)
+    
+    for achievement in Achievement.objects.all():
+        try:
+            UserAchievement.objects.get(user = user, achievement = achievement)
+            dictOfAchievements.append({"name" : achievement.name, "challenge" : achievement.challenge, "has" : True})
+        except:
+            dictOfAchievements.append({"name" : achievement.name, "challenge" : achievement.challenge, "has" : False})
+
+    return dictOfAchievements
+
 
 def totalFilledBottlesAchievementCheck(current_username):
     user = User.objects.get(username = current_username)
     bottles = user.bottles
-    listOfAchievements = UserAchievement.objects.all()
+    listOfNewAchievements = []
+
     listOfBottleCheckpoints = [1,5,10,50,100,250,500,1000]
 
     for checkpoint in listOfBottleCheckpoints:
@@ -31,7 +50,16 @@ def totalFilledBottlesAchievementCheck(current_username):
                 newUserAchievement = UserAchievement.objects.create(
                     user = user,
                     achievement = achievement)
+                listOfNewAchievements.append(newUserAchievement)
             
                 user.xp = user.xp + achievement.xp_reward
                 user.points = user.points + achievement.points_reward
                 user.save()
+    
+    dictOfNewAchievements = {"data" : []}
+    for newAchievement in listOfNewAchievements:
+        print(newAchievement.achievement.challenge)
+        dictOfNewAchievements["data"].append({"name" : newAchievement.achievement.name, 
+                                              "challenge" :newAchievement.achievement.challenge})
+
+    return dictOfNewAchievements
