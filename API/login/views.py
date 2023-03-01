b305f07a0aa40f1e0af3c1e7cba3b8d2002093db
Johan Schoultz <jkmschoultz@gmail.com>
2023-03-01 14:55:56 +0000

    Redirect create user to retrieve token

--- API/login/views.py
+++ API/login/views.py
@@ -3,6 +3,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import RegisterUser
 from rest_framework.permissions import AllowAny
+from rest_framework import status, exceptions
+from django.http import HttpResponse, JsonResponse
+from rest_framework.authentication import get_authorization_header, BaseAuthentication
+from database.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+import jwt
 
 class CreateUser(APIView):
     permission_classes = [AllowAny]
@@ -12,5 +18,9 @@ class CreateUser(APIView):
         if reg_serializer.is_valid():
             newuser = reg_serializer.save()
             if newuser:
-                return Response(status=status.HTTP_201_CREATED)
+                refresh = RefreshToken.for_user(newuser)
+                return JsonResponse({
+                    'token': str(refresh.access_token),
+                    'refresh': str(refresh)
+                })
         return Response(reg_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
