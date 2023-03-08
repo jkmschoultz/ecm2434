f3e78f231a5493ad6d1b28ca6b69feb48f36d59d
George Overton <81822610+GTX-Savage@users.noreply.github.com>
2023-03-08 12:28:57 +0000

    Added achievments to user endpoint

--- API/users/views.py
+++ API/users/views.py
@@ -1,21 +1,26 @@
 import math
 from database.models import User, UserAchievement, Achievement
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from achievements.views import getAllUserAchievements
 
+'''@csrf_exempt
 def index(request):
     print("here the request")
     if request.method == 'POST':
         body_unicode = request.body.decode('utf-8')
         body_data = json.loads(body_unicode)
-        name = str(body_data.get('name'))
-        username = str(body_data.get('username'))
-        password = str(body_data.get('password'))
+##        name = str(body_data.get('name'))
+##        username = str(body_data.get('username'))
+##        password = str(body_data.get('password'))
+        name 
+
         print(name)
         print(username)
         u = User(username = username, name = name, password = password,
             xp = 0, points = 0, has_been_verified = False)
         u.save()
-
+'''
 def UserRef(new_username, new_password):
     u = User(username = new_username, name = new_username, password = new_password,
             xp = 0, points = 0, has_been_verified = False)
@@ -47,7 +52,8 @@ def getUserProfileData(request, current_username):
     level = int(User.objects.get(username = current_username).level)
     xpLeft = int(User.objects.get(username = current_username).xpLeft)
     points = User.objects.get(username = current_username).points
-    return JsonResponse({"username":name, "Level":level, "XP":xpLeft, "streak":0, "points": points})
+    achievement = getAllUserAchievements(current_username)
+    return JsonResponse({"name":name, "Level":level, "XP":xpLeft, "streak":0, "points": points, "achievements": achievement})
 
     
 
