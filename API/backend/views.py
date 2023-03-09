903f977ce8937aec3dda4e5299bebe26e6107349
Johan Schoultz <jkmschoultz@gmail.com>
2023-03-09 17:45:39 +0000

    Added initial tests

--- API/backend/views.py
+++ API/backend/views.py
@@ -2,8 +2,8 @@ from django.http import JsonResponse
 from datetime import datetime
 
 def index(request):
-    current_time = datetime.now().strftime("%-I:%S %p")
-    current_date = datetime.now().strftime("%A %m %-Y")
+    current_time = datetime.now().strftime("%H:%M:%S")
+    current_date = datetime.now().strftime("%d/%m/%Y")
 
     data = {
         'time': current_time,
