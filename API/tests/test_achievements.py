138ab95d9330b1ba471c40c2f468d746a3e2e73f
TobySlump <toby.slump@gmail.com>
2023-03-13 13:08:10 +0000

    building achievements tests

--- API/tests/test_achievements.py
+++ API/tests/test_achievements.py
@@ -1,5 +1,12 @@
+'''
+This module tests all functionality and validation for the achievements feature
+'''
+
 from django.test import TestCase, Client
-from database.models import Achievement,UserAchievement,User,Building
+from database.models import Achievement,UserAchievement,User,Building, FilledBottle
+import datetime
+from django.utils import timezone
+import pytz
 
 # Create test cases for testing achievement functionality here
 class TestAchievements(TestCase):
@@ -12,6 +19,10 @@ class TestAchievements(TestCase):
         Building.objects.create(name="Test Building",
                                 latitude=5,
                                 longitude=5,
+                                radius=1)
+        Building.objects.create(name="Test Building 2",
+                                latitude=10,
+                                longitude=10,
                                 radius=1)
 
     def tearDown(self):
@@ -32,7 +43,7 @@ class TestAchievements(TestCase):
         # each achievement category is tested to make sure all their related achievements are created
         self.assertTrue({"challenge" : "Fill up a bottle every day for a month"} in response.json().get("data"))
         self.assertTrue({"challenge" : "Fill up 10 bottles"} in response.json().get("data"))
-        self.assertTrue({"challenge" : "Fill up 100 bottles in Test Building"} in response.json().get("data"))
+        self.assertTrue({"challenge" : "Fill up 100 bottles in Test Building 2"} in response.json().get("data"))
 
     def testFillAchievementsEndpointFull(self):
         # Test that the database is filled with no new achievements when it has already been filled 
@@ -92,3 +103,54 @@ class TestAchievements(TestCase):
         self.assertTrue("Fill up your first water bottle" in listOfNewAchievements)
         self.assertTrue("Fill up 50 bottles" in listOfNewAchievements)
         self.assertFalse("Fill up 100 bottles" in listOfNewAchievements)
+
+
+    def testBuildingAchievementsCorrect(self):
+        # Test that the user gets the correct achievements after reaching checkpoints within a building
+
+        # have the user fill up 5 bottles in the Test Building so that they should recieve this achievement
+        user = User.objects.get(username="AchievementsTestUser")
+        building = Building.objects.get(name="Test Building")
+        FilledBottle.objects.create(user=user, building=building, day=datetime.datetime(2023,2,12,17,5,7,tzinfo=pytz.UTC))
+        FilledBottle.objects.create(user=user, building=building, day=datetime.datetime(2023,2,17,17,5,7,tzinfo=pytz.UTC))
+        FilledBottle.objects.create(user=user, building=building, day=datetime.datetime(2023,2,25,17,5,7,tzinfo=pytz.UTC))
+        FilledBottle.objects.create(user=user, building=building, day=datetime.datetime(2023,3,4,17,5,7,tzinfo=pytz.UTC))
+        FilledBottle.objects.create(user=user, building=building, day=datetime.datetime(2023,3,7,17,5,7,tzinfo=pytz.UTC))
+
+        # fills the achievements table so that they can be checked against the user
+        c = Client()
+        c.get('/achievements/fill/AchievementsTestUser/')
+        response = c.get('/achievements/check/AchievementsTestUser/')
+
+        # generate a list of all the new achievement names generated
+        listOfNewAchievements = []
+        for newAchievement in response.json().get("data"):
+            listOfNewAchievements.append(newAchievement.get("challenge"))
+
+        self.assertTrue("Fill up 5 bottles in Test Building" in listOfNewAchievements)
+
+    def testBuildingAchievementsIncorrect(self):
+        # Test that the user does not get a building achievement when filling bottles in different buildings
+
+        # have the user fill up 5 bottles in total but split between the two test buildings so
+        # that they should not recieve an achievement
+        user = User.objects.get(username="AchievementsTestUser")
+        building = Building.objects.get(name="Test Building")
+        building2 = Building.objects.get(name="Test Building 2")
+        FilledBottle.objects.create(user=user, building=building, day=datetime.datetime(2023,2,12,17,5,7,tzinfo=pytz.UTC))
+        FilledBottle.objects.create(user=user, building=building2, day=datetime.datetime(2023,2,17,17,5,7,tzinfo=pytz.UTC))
+        FilledBottle.objects.create(user=user, building=building2, day=datetime.datetime(2023,2,25,17,5,7,tzinfo=pytz.UTC))
+        FilledBottle.objects.create(user=user, building=building, day=datetime.datetime(2023,3,4,17,5,7,tzinfo=pytz.UTC))
+        FilledBottle.objects.create(user=user, building=building2, day=datetime.datetime(2023,3,7,17,5,7,tzinfo=pytz.UTC))
+
+        # fills the achievements table so that they can be checked against the user
+        c = Client()
+        c.get('/achievements/fill/AchievementsTestUser/')
+        response = c.get('/achievements/check/AchievementsTestUser/')
+
+        # generate a list of all the new achievement names generated
+        listOfNewAchievements = []
+        for newAchievement in response.json().get("data"):
+            listOfNewAchievements.append(newAchievement.get("challenge"))
+
+        self.assertTrue(len(listOfNewAchievements) == 0)
