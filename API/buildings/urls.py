11b11f1e4fe9bcc628d5d7ebd7bd177ee8cf54d3
George Overton <81822610+GTX-Savage@users.noreply.github.com>
2023-03-14 17:27:40 +0000

    building tests

--- API/buildings/urls.py
+++ API/buildings/urls.py
@@ -8,5 +8,5 @@ app_name = 'buildings'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:building_id>/', views.detail, name='detail'),
-    path('<building_name>/leaderboard/', views.getTopFive, name='getTopFive'),
+    path('<building_id>/leaderboard/', views.getTopFive, name='detail'),
 ]
