94029a392bc1d5c911495e12f943050ff865e491
TobySlump <toby.slump@gmail.com>
2023-03-14 17:43:16 +0000

    allAvailable endpoint function test

--- API/tests/test_shop.py
+++ API/tests/test_shop.py
@@ -3,7 +3,7 @@ This module tests all functionality and validation for the user's points shop.
 '''
 
 from django.test import TestCase, Client
-from database.models import Achievement,UserAchievement,User,Building, FilledBottle
+from database.models import Achievement,UserAchievement,User,Building, FilledBottle, ShopItem, UserItem
 
 # Create test cases for testing shop functionality here
 class TestShop(TestCase):
@@ -12,19 +12,45 @@ class TestShop(TestCase):
         User.objects.create(username="TestUser",
                             email="TestUser@gmail.com",
                             name="TestName")
+        ShopItem.objects.create(name="Test Item 1",
+                                type="Border",
+                                cost=25)
+        ShopItem.objects.create(name="Test Item 2",
+                                type="Border",
+                                cost=15)
+        ShopItem.objects.create(name="Test Item 3",
+                                type="Background",
+                                cost=25)
 
     def tearDown(self):
         # Clean up run after every test method
         pass
 
     def testItemName(self):
-        # Test that stringified items are the name of the item
+        # Test that stringified items are the name of the item and the type of item
         pass
 
     def testAllAvailableEndpoint(self):
         # Test that the allAvailable shop endpoint returns a dictionary of every item not owned by the user
 
-        pass
+        # set the user points to 20 so only some items are affordable
+        user = User.objects.get(username="TestUser")
+        user.points = 20
+        user.save()
+
+        # have the user own one item already so it shouldn't be included in the response
+        item = ShopItem.objects.get(name="Test Item 3")
+        UserItem.objects.create(user=user, item=item)
+
+        c = Client()
+        response = c.get('/shop/available/TestUser/')
+
+        # only two items should be available and only one of them should be affordable for the TestUser
+        self.assertTrue(len(response.json().get("data")) == 2)
+        unaffordableItem = {"name":"Test Item 1", "item type" : "Border", "purchasable" : False}
+        affordableItem = {"name":"Test Item 2", "item type" : "Border", "purchasable" : True}
+        self.assertTrue(affordableItem in response.json().get("data"))
+        self.assertTrue(unaffordableItem in response.json().get("data"))
 
     def testPurachasableBoolean(self):
         # Test that the boolean ,returned with each item when the allAvailable endpoint is called, is correct
