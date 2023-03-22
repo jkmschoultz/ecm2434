71f76b53c4164c0d92cf08c52c28ecf2e8c9d309
qwazzarr <khmaridze@mail.ru>
2023-03-22 01:29:50 +0000

    Made working profile background border images and username data

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -7,7 +7,7 @@ const axiosInstance = axios.create({
     timeout: 5000,
     headers: {
         Authorization: localStorage.getItem('access_token')
-            ? 'JWT ' + localStorage.getItem('access_token')
+                ? 'Bearer ' + localStorage.getItem('access_token')
             : null,
         'Content-Type': 'application/json',
         accept: 'application/json',
@@ -32,7 +32,7 @@ axiosInstance.interceptors.response.use(
 
         if (
             error.response.status === 401 &&
-            originalRequest.url === baseURL + 'auth/token/refresh/'
+            originalRequest.url === baseURL + 'auth/token/refresh'
         ) {
             window.location.href = '/';
             return Promise.reject(error);
@@ -60,9 +60,9 @@ axiosInstance.interceptors.response.use(
                             localStorage.setItem('refresh_token', response.data.refresh);
 
                             axiosInstance.defaults.headers['Authorization'] =
-                                'JWT ' + response.data.access;
+                                'Bearer ' + response.data.access;
                             originalRequest.headers['Authorization'] =
-                                'JWT ' + response.data.access;
+                                'Bearer ' + response.data.access;
 
                             return axiosInstance(originalRequest);
                         })
--- frontend/src/pages/profile/profile.js
+++ frontend/src/pages/profile/profile.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Navbar from '../../components/navbar';
+import photo from '../../assets/useravatar.png';
 
-import classes from "./profile.module.css";
-import Navbar from "../../components/navbar";
-import photo from "../../assets/useravatar.png";
-
+import classes from './profile.module.css';
+import axiosInstance from "../../axios";
 
 const Profile = () => {
-
     //get list of achievements ,and whether they are completed or not
     //get user info
+    const [items, setItems] = useState(null);
 
+    useEffect(() => {
+        // Fetch items from backend based on current itemType
+        console.log("Updating items: " + localStorage.getItem('access_token'));
+        axiosInstance.get(`users/data`)
+            .then(response => {
+                console.log(response);
+                setItems(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
+    if(!items){
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
     return (
         <div className={classes.background}>
             <header>
@@ -20,37 +40,41 @@ const Profile = () => {
                 <link rel="stylesheet" href="style.css" />
             </head>
             <div className={classes.container}>
-                <div className={classes.sidebar}>
-                    <div className={classes.profile}>
-                    <img className={classes.profileImg} src={photo} alt="Profile Picture of the user" />
+                <div className={classes.sidebar} style={{borderImage:`url(http://${items.profile_border}) 20% repeat`}}>
+                    <div className={classes.profile} style={{ background: `url(http://${items.profile_background})` , backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                        <div className={classes.profileImgContainer}>
+                            <img className={classes.profileImg} src={"http://"+items.profile_pic} alt="Profile Picture of the user" />
+                        </div>
                         <div className={classes.profileText}>
-                        <h2>Username</h2>
-                        <p>Full Name</p>
-                        <p>Email</p>
-                        <p>Course Title</p>
+                            <h2>{items.username}</h2>
+                            <div className={classes.quoteContainer}>
+                                <h3>Quote of the day</h3>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className={classes.main}>
                     <div className={classes.achievements}>
-                        <h2 className={classes.achievements}>Achievements</h2>
-                        <button className={classes.achievementButton}>
-                            You filled your first water bottle!
-                            <span className={classes.star1}></span>
-                        </button>
-                        <button className={classes.achievementButton}>
-                            You filled water bottle 50 times!
-                            <span className={classes.star2}></span>
-                        </button>
-                        <button className={classes.achievementButton}>
-                            You are in top 10 on the leaderboard!
-                            <span className={classes.star3}></span>
-                        </button>
-                    </div>   
+                        <h2>Achievements</h2>
+                        <div className={classes.achievementContainer}>
+                            <button className={classes.achievementButton}>
+                                You filled your first water bottle!
+                                <span className={classes.star1}></span>
+                            </button>
+                            <button className={classes.achievementButton}>
+                                You filled water bottle 50 times!
+                                <span className={classes.star2}></span>
+                            </button>
+                            <button className={classes.achievementButton}>
+                                You are in top 10 on the leaderboard!
+                                <span className={classes.star3}></span>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>    
-    )
-}
+        </div>
+    );
+};
 
 export default Profile;
