ec6c042d22265b65417cb700b00d84c31e184b1b
George Overton <81822610+GTX-Savage@users.noreply.github.com>
2023-03-22 11:44:14 +0000

    a test for quiz

--- API/tests/test_quiz.py
+++ API/tests/test_quiz.py
@@ -33,4 +33,25 @@ class TestQuiz(TestCase):
         self.assertEqual(len(leaderboard), 1)
         entry = leaderboard[0]
         self.assertEqual(entry.user_points_in_building, 15)  # 15 points for 3 right answers
+
+    def testQuizNewUser(self):
+        # Test that user answers to quiz are correctly processed
+        building_test = Building.objects.get(name = 'test')
+        # Verify leaderboard exists in building
+        leaderboard = Leaderboard.objects.filter(building = building_test)
+        self.assertEqual(len(leaderboard), 1)
+
+        # Simulate a new user answering to the quiz
+        user = User.objects.create(username = "unitTestUser2", email = "test2@gmail.com", password="jibba jabba", name = "unitTest",
+            xp = 20, points = 7, has_been_verified=False)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        data = {'correct':3,'building': 'test'}  # Sample quiz result data
+        c.post("/quiz/", data=data)
+
+        # Verify leaderboard has been updated with a new entery and ordered lowest to highest
+        leaderboard = Leaderboard.objects.filter(building = building_test)
+        self.assertEqual(len(leaderboard), 2)
+        entry = leaderboard[1]
+        self.assertEqual(entry.user_points_in_building, 15)  # 15 points for 3 right answers
         
